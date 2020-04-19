732f5c95f685f7acc7aa402adbd87ed7a3d7a7b4
sheikhafsar <sheikhafsar72@gmail.com>
2020-04-19 22:07:55 +0530

    ejs view engine implementation

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const http = require('http');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 
 const app = express();
-
+/*
 app.engine(
     'hbs',
     expressHbs({
@@ -16,8 +16,8 @@ app.engine(
         defaultLayout:'main-layout', 
         extname:'hbs'
     })
-);
-app.set('view engine', 'hbs'); //hbs -> extensions
+);*/
+app.set('view engine', 'ejs'); //hbs -> extensions
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -31,7 +31,7 @@ app.use(shopRoutes);
 
 app.use( (req,res,next) =>{
     //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.status(404).render('404',{pageTitle:'Page Not Found'});
+    res.status(404).render('404', { pageTitle:'Page Not Found' });
 });
 
 app.listen(3000);
